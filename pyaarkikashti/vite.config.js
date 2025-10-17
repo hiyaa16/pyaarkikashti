392c1cfe1fbec6c0152b8deeac71e1991bdb5389
Hiya Jain <Hiyajain975@gmail.com>
2025-10-17 11:16:14 +0530

    .

--- pyaarkikashti/vite.config.js
+++ pyaarkikashti/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', 
+});
